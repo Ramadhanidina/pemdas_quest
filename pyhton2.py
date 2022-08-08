fa921f5a2d7dc72d5cf1af1f79057a943eba0189
Ramadhanidina <110509003+Ramadhanidina@users.noreply.github.com>
2022-08-07 18:51:15 -0700

    Add files via upload

---
+++ pyhton2.py
@@ -0,0 +1,11 @@
+# belajar input
+
+nama ="Dina" # Dina adalah data, nama adalah variabel
+umur = 15
+Alamat = "cimanggis, kota depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("nama saya adalah " + nama)
---
+++ python1.py
@@ -0,0 +1,11 @@
+# belajar Qutput
+
+print("Hello world")
+print("nama saya Dina")
+print("saya sekolah di smk harapan bangsa")
+print("umur saya 15 tahun ")
+print("saya suka membaca wattpad")
+print("saya tinggal di mekarsari cimanggis")
+print("banyakin bersyukur ya")
+print("jangan banyak ngeluh")
+print("pasti allah kasih jalan keluarnya")
